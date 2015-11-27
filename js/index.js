95e98d74932615bf67bf5c92c0716b2234200c6d
alvin zhang <surualv@hotmail.com>
2015-11-28 00:45:05 +0800

    MOD: mobile 适配

---
+++ js/index.js
@@ -0,0 +1,31 @@
+var minWebWidth = 769;
+
+var UA = navigator.userAgent,
+	IsAndroid = /Android|HTC/i.test(UA) || !!(navigator["platform"] + "").match(/Linux/i),
+	IsIOS = !IsAndroid && /iPad|iPod|iPhone/i.test(UA);
+
+var isMobile = determineDevice();
+
+/**
+	* determine whether it is a mobile device
+	*/
+function determineDevice() {
+	var deviceWidth = (IsAndroid || IsIOS) ? $('body').width() : window.innerWidth;
+	return IsAndroid || IsIOS || deviceWidth < minWebWidth;
+}
+
+$(document).ready(function () {
+	$('.gallary').on('click', function () {
+		if (isMobile) {
+			if ($('#float-algo').hasClass('shown')) {
+				$('#float-algo').removeClass('shown');
+				$('#float-js').removeClass('shown');
+				$('#float-pt').removeClass('shown');
+			} else {
+				$('#float-algo').addClass('shown');
+				$('#float-js').addClass('shown');
+				$('#float-pt').addClass('shown');
+			}
+		}
+	});
+});
